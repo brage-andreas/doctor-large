2405149c1ef73e0a93062ff326e6de28faffcaab
Drango <draaangooo@gmail.com>
2022-12-07 23:59:01 +0100

    feat: toEmbed

--- src/database/giveaway.ts
+++ src/database/giveaway.ts
@@ -1,4 +1,8 @@
-import { type Prisma } from "@prisma/client";
+import { type giveaway, type Prisma } from "@prisma/client";
+import { oneLine, stripIndents } from "common-tags";
+import { EmbedBuilder, type Client } from "discord.js";
+import { listify } from "../helpers/listify.js";
+import { timestamp } from "../helpers/timestamps.js";
 import prisma from "./prisma.js";
 
 export default class GiveawayManager {
@@ -56,5 +60,57 @@ export default class GiveawayManager {
 
 	public async editPrize(args: Prisma.giveawayPrizeUpdateArgs) {
 		return await this.prisma.giveawayPrize.update(args);
+	}
+
+	public toEmbed(data: giveaway, client: Client) {
+		const lockedStr = data.lockEntries
+			? "🔒 Entries are currently locked."
+			: null;
+
+		const roleGateStr = data.requiredRoles.length
+			? "🔒 You need specific roles to enter this Giveaway."
+			: null;
+
+		const roles = client.guilds.cache
+			.get(data.guildId)
+			?.roles.cache.filter((role) => data.requiredRoles.includes(role.id))
+			.sort((a, b) => a.position - b.position)
+			.map((role) => role.toString());
+
+		const rolesStr = roles && listify(roles, { length: 5 });
+
+		const timestampInfo = data.endTimestamp
+			? oneLine`
+				• The Giveaway closes
+				${timestamp(data.endTimestamp, "F")}
+				(${timestamp(data.endTimestamp, "R")}).
+				Until then, cross your fingers 🤞
+			`
+			: "• The Giveaway is open indefinitely. Enter while you can!";
+
+		const numberOfWinnersInfo = oneLine`
+			• There can be ${data.numberOfWinners}
+			winner${data.numberOfWinners === 1 ? "" : "s"}! 😁
+		`;
+
+		const requiredRolesInfo =
+			rolesStr && `• Roles required to enter: ${rolesStr}`;
+
+		return new EmbedBuilder()
+			.setTitle(data.giveawayTitle)
+			.setDescription(
+				stripIndents`
+					${lockedStr ? lockedStr : roleGateStr ? roleGateStr : ""}
+					
+					${data.giveawayDescription}
+
+					${timestampInfo}\n${numberOfWinnersInfo}\n${requiredRolesInfo ?? ""}`
+			)
+			.setFooter({ text: `${data.hostUserTag} (${data.hostUserId})` })
+			.setTimestamp(Number(data.createdTimestamp))
+			.addFields({
+				name: "Prizes",
+				value: "WIP"
+			});
 	}
 }
