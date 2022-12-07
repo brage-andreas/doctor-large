96cb74cdba751a91bcfbf2df22b1d0ccda15e6d3
Drango <draaangooo@gmail.com>
2022-12-07 23:59:19 +0100

    feat: listify

---
+++ src/helpers/listify.ts
@@ -0,0 +1,26 @@
+export const listify = (
+	elements: Array<string>,
+	options: { length: number; give?: number }
+): string => {
+	const { length, give } = options ?? {};
+	const elements_ = structuredClone(elements);
+
+	if (elements_.length === 0 || elements_.length === 1) {
+		return elements_.join("");
+	}
+
+	if (length + (give ?? 1) < elements_.length) {
+		elements_.splice(length);
+		elements_.push(`and ${elements.length - length} more`);
+
+		return elements_.join(", ");
+	}
+
+	if (elements_.length === 2) {
+		return `${elements_[0]} and ${elements_[1]}`;
+	}
+
+	const lastElement = elements_.splice(-1);
+
+	return `${elements_.join(", ")}, and ${lastElement[0]}`;
+};
