001cf250fb31dcf49df745ad9133412d0919db6c
el-durango <el-durango@gmail.com>
2022-12-09 21:20:17 +0100

    feat: add components

--- src/components/giveaway.ts
+++ src/components/giveaway.ts
@@ -60,10 +60,137 @@ const openInDashboardButton = new ButtonBuilder()
 	.setStyle(ButtonStyle.Secondary);
 
 // -----------------------
+//          EDIT
+// -----------------------
+
+const modalGiveawayNewTitle = (oldTitle: string) =>
+	new TextInputBuilder()
+		.setCustomId("new-title")
+		.setLabel("New Title")
+		.setMaxLength(50)
+		.setStyle(TextInputStyle.Short)
+		.setRequired(true)
+		.setValue(oldTitle)
+		.setPlaceholder(oldTitle);
+
+const emptyString = "😴 Whoa so empty — there is no description";
+
+const modalGiveawayNewDescription = (oldDescription: string | null) =>
+	new TextInputBuilder()
+		.setCustomId("new-description")
+		.setLabel("New description [None]")
+		.setMaxLength(512)
+		.setStyle(TextInputStyle.Paragraph)
+		.setValue(oldDescription ?? emptyString)
+		.setPlaceholder(oldDescription ?? emptyString);
+
+const modalGiveawayNewNumberOfWinners = (oldNumberOfWinners: number) =>
+	new TextInputBuilder()
+		.setCustomId("new-number-of-winners")
+		.setLabel("New number of winners [1]")
+		.setMaxLength(2)
+		.setStyle(TextInputStyle.Short)
+		.setValue(oldNumberOfWinners.toString())
+		.setPlaceholder(oldNumberOfWinners.toString());
+
+const editOptionsModal = (
+	id: number,
+	oldTitle: string,
+	oldDescription: string | null,
+	oldNumberOfWinners: number
+) =>
+	new ModalBuilder()
+		.setTitle(`Edit giveaway #${id} (3 min)`)
+		.setCustomId("giveawayEdit")
+		.setComponents(
+			new ActionRowBuilder<TextInputBuilder>().addComponents(
+				modalGiveawayNewTitle(oldTitle)
+			),
+			new ActionRowBuilder<TextInputBuilder>().addComponents(
+				modalGiveawayNewDescription(oldDescription)
+			),
+			new ActionRowBuilder<TextInputBuilder>().addComponents(
+				modalGiveawayNewNumberOfWinners(oldNumberOfWinners)
+			)
+		);
+
+// -----------------------
+//        DASHBOARD
+// -----------------------
+
+const publishGiveawayButton = new ButtonBuilder()
+	.setCustomId("publishGiveaway")
+	.setStyle(ButtonStyle.Success)
+	.setLabel("Publish");
+
+const republishGiveawayButton = new ButtonBuilder()
+	.setCustomId("republishGiveaway")
+	.setStyle(ButtonStyle.Success)
+	.setLabel("Republish");
+
+const lockGiveawayEntriesButton = new ButtonBuilder()
+	.setCustomId("lockEntries")
+	.setStyle(ButtonStyle.Secondary)
+	.setLabel("Lock entries");
+
+const unlockGiveawayEntriesButton = new ButtonBuilder()
+	.setCustomId("unlockEntries")
+	.setStyle(ButtonStyle.Secondary)
+	.setLabel("Unlock entries");
+
+const setRequiredRolesButton = new ButtonBuilder()
+	.setCustomId("setRequiredRoles")
+	.setStyle(ButtonStyle.Secondary)
+	.setLabel("Set roles");
+
+const clearRequiredRolesButton = new ButtonBuilder()
+	.setCustomId("clearRequiredRoles")
+	.setStyle(ButtonStyle.Secondary)
+	.setLabel("Clear roles");
+
+const editGiveawayButton = new ButtonBuilder()
+	.setCustomId("giveawayEditButton")
+	.setStyle(ButtonStyle.Secondary)
+	.setLabel("Edit");
+
+const manageGiveawayPrizesButton = new ButtonBuilder()
+	.setCustomId("prizesManage")
+	.setStyle(ButtonStyle.Secondary)
+	.setLabel("Manage prizes");
+
+const endGiveawayButton = new ButtonBuilder()
+	.setCustomId("endGiveaway")
+	.setStyle(ButtonStyle.Danger)
+	.setLabel("End giveaway");
+
+// -----------------------
 
 export const giveaway = {
 	create: {
-		optionsModal: () => structuredClone(createOptionsModal),
-		dashboardButton: () => structuredClone(openInDashboardButton)
+		optionsModal: () => createOptionsModal,
+		dashboardButton: () => openInDashboardButton
+	},
+	edit: {
+		editOptionsModal: (
+			id: number,
+			oldTitle: string,
+			oldDescription: string | null,
+			oldNumberOfWinners: number
+		) => editOptionsModal(id, oldTitle, oldDescription, oldNumberOfWinners)
+	},
+	dashboard: {
+		row1: {
+			publishButton: () => publishGiveawayButton,
+			republishButton: () => republishGiveawayButton,
+			lockEntriesButton: () => lockGiveawayEntriesButton,
+			unlockEntriesButton: () => unlockGiveawayEntriesButton,
+			setRolesButton: () => setRequiredRolesButton,
+			clearRolesButton: () => clearRequiredRolesButton
+		},
+		row2: {
+			editButton: () => editGiveawayButton,
+			endButton: () => endGiveawayButton,
+			managePrizesButton: () => manageGiveawayPrizesButton
+		}
 	}
 };
