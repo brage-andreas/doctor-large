744b007838766bd3a46b88165eeb2a034e308ed9
Drango <draaangooo@gmail.com>
2023-01-07 21:36:03 +0100

    feat: remove now unused types

--- src/typings/database.d.ts
+++ src/typings/database.d.ts
@@ -3,13 +3,3 @@ import type { GiveawayData, PrizeData, WinnerData } from "@prisma/client";
 export type GiveawayDataWithIncludes = GiveawayData & {
 	prizes: Array<PrizeData & { winners: Array<WinnerData> }>;
 };
-
-export type PrizeDataWithIncludes = PrizeData & {
-	giveaway: GiveawayData;
-	winners: Array<WinnerData>;
-};
-
-type WonPrize = Exclude<PrizeData, "winners"> & {
-	quantityWon: number;
-	accepted: boolean;
-};
