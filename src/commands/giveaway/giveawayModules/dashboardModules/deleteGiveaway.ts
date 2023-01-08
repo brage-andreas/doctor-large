6f0589a5e4509c5cfa79ef43f76d322d7529221c
Drango <draaangooo@gmail.com>
2023-01-08 23:16:37 +0100

    wip: deleteGiveaway

--- src/commands/giveaway/giveawayModules/dashboardModules/deleteGiveaway.ts
+++ src/commands/giveaway/giveawayModules/dashboardModules/deleteGiveaway.ts
@@ -1,4 +1,6 @@
+import { stripIndents } from "common-tags";
 import { type ButtonInteraction } from "discord.js";
+import { EMOJIS } from "../../../../constants.js";
 import type GiveawayManager from "../../../../database/giveaway.js";
 import toDashboard from "../dashboard.js";
 
@@ -10,6 +12,17 @@ export default async function toDeleteGiveaway(
 	const giveaway = await giveawayManager.get(id);
 
 	if (!giveaway) {
+		await interaction.reply({
+			components: [],
+			ephemeral: true,
+			content: stripIndents`
+				How did we get here?
+			
+				${EMOJIS.WARN} This giveaway does not exist. Try creating one or double-check the ID.
+			`,
+			embeds: []
+		});
+
 		return;
 	}
 
