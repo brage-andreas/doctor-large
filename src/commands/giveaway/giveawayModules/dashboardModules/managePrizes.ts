454e6a53b2044a90b1f8f7f99f5c2ac779c17484
el-durango <el-durango@gmail.com>
2023-01-11 21:26:34 +0100

    wip: managePrizes

--- src/commands/giveaway/giveawayModules/dashboardModules/managePrizes.ts
+++ src/commands/giveaway/giveawayModules/dashboardModules/managePrizes.ts
@@ -1,4 +1,6 @@
+import { stripIndents } from "common-tags";
 import { type ButtonInteraction } from "discord.js";
+import { EMOJIS } from "../../../../constants.js";
 import type GiveawayManager from "../../../../database/giveaway.js";
 import toDashboard from "../dashboard.js";
 
@@ -10,6 +12,16 @@ export default async function toManagePrizes(
 	const giveaway = await giveawayManager.get(id);
 
 	if (!giveaway) {
+		await interaction.editReply({
+			components: [],
+			content: stripIndents`
+				How did we get here?
+			
+				${EMOJIS.WARN} This giveaway does not exist. Try creating one or double-check the ID.
+			`,
+			embeds: []
+		});
+
 		return;
 	}
 
