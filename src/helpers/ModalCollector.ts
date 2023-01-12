c0a1643468e497dc00b6df3fd3542e4be517aded
el-durango <el-durango@gmail.com>
2023-01-12 13:58:01 +0100

    feat: ModalCollector

---
+++ src/helpers/ModalCollector.ts
@@ -0,0 +1,33 @@
+import {
+	InteractionCollector,
+	InteractionType,
+	type Interaction,
+	type ModalBuilder,
+	type ModalSubmitInteraction,
+	type ModalSubmitInteractionCollectorOptions
+} from "discord.js";
+
+export const modalId = () => `modal-${Date.now().toString().slice(-5)}`;
+
+export const ModalCollector = (
+	interaction: Interaction<"cached">,
+	modal: ModalBuilder,
+	collectorOptions?: Exclude<
+		ModalSubmitInteractionCollectorOptions<ModalSubmitInteraction>,
+		"filter" | "interactionType" | "max"
+	>
+) =>
+	new InteractionCollector<ModalSubmitInteraction<"cached">>(
+		interaction.client,
+		{
+			interactionType: InteractionType.ModalSubmit,
+			filter: (modalInteraction) =>
+				modalInteraction.user.id === interaction.user.id &&
+				modalInteraction.customId === modal.data.custom_id,
+			channel: interaction.channelId ?? undefined,
+			guild: interaction.guildId,
+			time: 60_000,
+			max: 1,
+			...collectorOptions
+		}
+	);
