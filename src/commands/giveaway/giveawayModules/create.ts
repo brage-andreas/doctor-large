b5cbc27a655af29a0f2207670be70f588dcc3ff9
Drango <draaangooo@gmail.com>
2023-01-15 00:47:40 +0100

    feat: use modal collector,

--- src/commands/giveaway/giveawayModules/create.ts
+++ src/commands/giveaway/giveawayModules/create.ts
@@ -1,59 +1,56 @@
 import { type ChatInputCommandInteraction } from "discord.js";
 import { giveawayComponents } from "../../../components/index.js";
 import GiveawayManager from "../../../database/giveaway.js";
+import { ModalCollector } from "../../../helpers/ModalCollector.js";
 import Logger from "../../../logger/logger.js";
 import toDashboard from "./dashboard.js";
 
 export default async function (
 	interaction: ChatInputCommandInteraction<"cached">
 ) {
-	await interaction.showModal(giveawayComponents.create.optionsModal());
+	const modal = giveawayComponents.create.optionsModal();
 
-	const modalInteraction = await interaction
-		.awaitModalSubmit({
-			filter: (interaction) => interaction.customId === "createGiveaway",
-			time: 180_000
-		})
-		.catch(async () => {
-			await interaction.followUp({
-				ephemeral: true,
-				content:
-					"Something went wrong. The time limit is 3 minutes. Try again!"
-			});
-		});
+	await interaction.showModal(modal);
 
-	if (!modalInteraction?.fields.fields.size) {
-		return;
-	}
-
-	const giveawayManager = new GiveawayManager(interaction.guild);
-
-	const title = modalInteraction.fields.getTextInputValue("title");
-
-	const description =
-		modalInteraction.fields.getTextInputValue("description");
-
-	const winnerQuantity = Number(
-		modalInteraction.fields.getTextInputValue("winnerQuantity")
-	);
-
-	await modalInteraction.deferReply({ ephemeral: true });
-
-	const nextGuildRelativeId = await giveawayManager.getNextGuildRelativeId();
-
-	const { id } = await giveawayManager.create({
-		guildRelativeId: nextGuildRelativeId,
-		winnerQuantity,
-		hostUserTag: interaction.user.tag,
-		hostUserId: interaction.user.id,
-		description,
-		guildId: interaction.guildId,
-		title
+	const collector = ModalCollector(interaction, modal, {
+		time: 180_000
 	});
 
-	new Logger({ prefix: "GIVEAWAY", interaction }).log(
-		`Created giveaway with ID #${id}`
-	);
+	collector.on("collect", async (modalInteraction) => {
+		await modalInteraction.deferReply({ ephemeral: true });
 
-	toDashboard(modalInteraction, id);
+		if (!modalInteraction?.fields.fields.size) {
+			return;
+		}
+
+		const giveawayManager = new GiveawayManager(interaction.guild);
+
+		const title = modalInteraction.fields.getTextInputValue("title");
+
+		const description =
+			modalInteraction.fields.getTextInputValue("description");
+
+		const winnerQuantity = Number(
+			modalInteraction.fields.getTextInputValue("winnerQuantity")
+		);
+
+		const nextGuildRelativeId =
+			await giveawayManager.getNextGuildRelativeId();
+
+		const { id } = await giveawayManager.create({
+			guildRelativeId: nextGuildRelativeId,
+			winnerQuantity,
+			hostUserTag: interaction.user.tag,
+			hostUserId: interaction.user.id,
+			description,
+			guildId: interaction.guildId,
+			title
+		});
+
+		new Logger({ prefix: "GIVEAWAY", interaction }).log(
+			`Created giveaway with ID #${id}`
+		);
+
+		toDashboard(modalInteraction, id);
+	});
 }
