f12e31a7c14b85b1bf2ce0997c116133669ebe98
Drango <draaangooo@gmail.com>
2023-01-22 23:35:00 +0100

    feat: add `PrizeWithIncludes`

--- src/typings/database.d.ts
+++ src/typings/database.d.ts
@@ -1,5 +1,11 @@
 import type { Giveaway, Prize, Winner } from "@prisma/client";
+import type GiveawayModule from "../modules/Giveaway.js";
 
 export type GiveawayWithIncludes = Giveaway & {
 	prizes: Array<Prize & { winners: Array<Winner> }>;
+};
+
+export type PrizeWithIncludes = Prize & {
+	winners: Array<Winner>;
+	giveaway: GiveawayModule;
 };
