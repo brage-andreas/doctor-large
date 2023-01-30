4d19e796feece07e9ed9bd946a28d820ef4bdd26
el-durango <el-durango@gmail.com>
2023-01-30 13:33:56 +0100

    feat: add giveaway constants and refactor

--- src/constants.ts
+++ src/constants.ts
@@ -5,65 +5,68 @@ export const COMMAND_DIR = new URL("./commands", import.meta.url);
 export const EVENT_DIR = new URL("./events", import.meta.url);
 
 export const REGEXP = {
+	ACCEPT_PRIZE_CUSTOM_ID: /^accept-prize-(?<id>\d+)$/,
 	DASHBOARD_PRIZE_CUSTOM_ID: /^dashboard-prize-(?<id>\d+)/,
 	ENTER_GIVEAWAY_CUSTOM_ID: /^enter-giveaway-(?<id>\d+)$/,
-	ACCEPT_PRIZE_CUSTOM_ID: /^accept-prize-(?<id>\d+)$/,
 	ID: /^\d{17,19}$/
 };
 
 // using discord's colours if possible
 // <https://discord.com/branding>
 export const COLORS = {
-	YELLOW: "#FEE75C",
 	GREEN: "#57F287",
-	RED: "#ED4245"
+	RED: "#ED4245",
+	YELLOW: "#FEE75C"
 } as const;
 
 export const EMOJIS = {
-	ENTER_GIVEAWAY_EMOJI: "🎁",
-	HEART_BREAK: "💔",
-	SWEAT_SMILE: "😅",
-	STAR_EYES: "🤩",
-	NO_ENTRY: "⛔",
-	PENSIVE: "🥺",
-	HIGHER: "🔺",
-	SPARKS: "✨",
-	UNLOCK: "🔓",
-	ENDED: "🔸",
-	ERROR: "<:Error:1061984813650804799>",
-	LOWER: "🔻",
-	SHUSH: "🤫",
-	SLEEP: "😴",
-	THINK: "🤔",
+	CRY: "😢",
 	EDIT: "✍️",
+	ENDED: "🔸",
+	ENTER_GIVEAWAY_EMOJI: "🎁",
+	ERROR: "<:Error:1061984813650804799>",
 	GRIN: "😁",
 	HALO: "😇",
+	HEART_BREAK: "💔",
+	HIGHER: "🔺",
 	LOCK: "🔒",
-	TADA: "🎉",
-	WARN: "<:Warning:1061984815781531729>",
-	CRY: "😢",
+	LOWER: "🔻",
+	NO_ENTRY: "⛔",
 	OFF: "<:Off:1047914155929256026>",
 	ON: "<:On:1047914157409828934>",
+	PENSIVE: "🥺",
+	SHUSH: "🤫",
+	SLEEP: "😴",
+	SPARKS: "✨",
+	STAR_EYES: "🤩",
+	SWEAT_SMILE: "😅",
+	TADA: "🎉",
+	THINK: "🤔",
+	UNLOCK: "🔓",
 	V: "<:Checkmark:1054081028962136104>",
+	WARN: "<:Warning:1061984815781531729>",
 	X: "<:Xmark:1054081030107185252>"
 } as const;
 
 export const INTENTS: Array<GatewayIntentBits> = [
-	GatewayIntentBits.GuildMessages,
 	GatewayIntentBits.GuildMembers,
+	GatewayIntentBits.GuildMessages,
 	GatewayIntentBits.Guilds
 ];
 
 export const GIVEAWAY = {
-	MAX_WINNER_QUANTITY_LEN: 2,
-	MAX_DESCRIPTION_LINES: 20,
+	END_HOST_DM_BEFORE_END: 1_800_000, // 30 minutes
 	MAX_DESCRIPTION_LEN: 200,
-	MAX_TITLE_LEN: 50
+	MAX_DESCRIPTION_LINES: 20,
+	MAX_TITLE_LEN: 50,
+	MAX_WINNER_QUANTITY_LEN: 2,
+	MIN_END_DATE_BUFFER: 600_000 // 10 minutes
 } as const;
 
 export const PRIZE = {
 	MAX_QUANTITY: 10, // -- MATCH THESE
 	MAX_QUANTITY_LEN: 2, // MATCH THESE
+
 	MAX_ADDITIONAL_INFO_LEN: 70,
 	MAX_TITLE_LEN: 30
 } as const;
