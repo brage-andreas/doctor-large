dfff2307cb1269bf7a19448ade257d330aaacf95
el-durango <draaangooo@gmail.com>
2023-02-25 19:14:36 +0100

    feat: message to embed

--- src/helpers/messageToEmbed.ts
+++ src/helpers/messageToEmbed.ts
@@ -8,14 +8,34 @@ export default function messageToEmbed(message: Message<true>) {
 			name: `${message.author.tag} (${message.author.id})`
 		})
 		.setColor(COLORS.EMBED_INVISIBLE)
-		.setDescription(message.content.length ? message.content : null)
 		.setFooter({ text: `#${message.channel.name}` })
 		.setTimestamp(message.createdAt);
+
+	const content: Array<string> = [];
+
+	if (message.embeds.length) {
+		const emb = message.embeds.find(
+			(embed) => embed.image || embed.thumbnail
+		);
+
+		const url = emb?.image?.url ?? emb?.thumbnail?.url;
+
+		if (url) {
+			embed.setImage(url);
+		}
+	}
 
 	if (message.attachments.size) {
 		const attachment = message.attachments.first();
 		const type = attachment?.contentType ?? "";
 		const name = attachment?.name ?? "";
+
+		content.push(
+			[...message.attachments.values()]
+				.map(({ url }, i) => `[Attachment ${i + 1}](<${url}>)`)
+				.join(" • "),
+			"---"
+		);
 
 		const validType = ["image/jpeg", "image/png", "image/gif"].includes(
 			type
@@ -29,6 +49,10 @@ export default function messageToEmbed(message: Message<true>) {
 			embed.setImage(attachment.url);
 		}
 	}
+
+	content.push(message.content || "*No content.*");
+
+	embed.setDescription(content.join("\n\n"));
 
 	return embed;
 }
