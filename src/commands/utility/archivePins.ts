93ca623ee66732bc48bb65ce8db755aa6484f4c4
el-durango <draaangooo@gmail.com>
2023-02-25 19:15:06 +0100

    feat: archive pins command

--- src/commands/utility/archivePins.ts
+++ src/commands/utility/archivePins.ts
@@ -1,27 +1,133 @@
-import { type Command, type CommandModuleInteractions } from "#typings";
+import components from "#components";
+import { EMOJIS } from "#constants";
+import messageToEmbed from "#helpers/messageToEmbed.js";
+import yesNo from "#helpers/yesNo.js";
+import { type Command, type CommandData } from "#typings";
+import { source } from "common-tags";
 import {
+	ActionRowBuilder,
 	ApplicationCommandType,
 	PermissionFlagsBits,
-	type RESTPostAPIApplicationCommandsJSONBody
+	type ButtonBuilder,
+	type ChannelSelectMenuBuilder,
+	type ChatInputCommandInteraction,
+	type ContextMenuCommandInteraction,
+	type NewsChannel,
+	type TextChannel
 } from "discord.js";
 
-const data: RESTPostAPIApplicationCommandsJSONBody = {
-	name: "archive_pins",
-	dm_permission: false,
-	description: "Automatically archive pins to a separate channel.",
-	default_member_permissions: PermissionFlagsBits.ManageMessages.toString(),
-	type: ApplicationCommandType.ChatInput
+const data: CommandData = {
+	commandName: "archive_pins",
+	contextMenu: {
+		name: "Archive 5 oldest pins",
+		dm_permission: false,
+		default_member_permissions:
+			PermissionFlagsBits.ManageMessages.toString(),
+		type: ApplicationCommandType.Message
+	}
 };
 
-const run = async (interaction: CommandModuleInteractions) => {
-	if (!interaction.isChatInputCommand()) {
-		return;
+const contextMenu = async (
+	interaction:
+		| ChatInputCommandInteraction<"cached">
+		| ContextMenuCommandInteraction<"cached">
+) => {
+	await interaction.deferReply({ ephemeral: true });
+
+	const allMessages = await interaction.channel?.messages
+		.fetchPinned()
+		.catch(() => null);
+
+	if (!allMessages?.size) {
+		return interaction.editReply({
+			content: `${EMOJIS.SWEAT_SMILE} There are no pins to archive.`
+		});
 	}
 
-	await interaction.deferReply({ ephemeral: true });
+	const messages = [...allMessages.values()]
+		.sort((a, b) => a.createdTimestamp - b.createdTimestamp)
+		.slice(0, 5);
+
+	const links = messages
+		.filter((m) => !m.deletable)
+		.map((m) => `→ [Message](<${m.url}>) by ${m.author.tag}`);
+
+	const missingPerms = links.length
+		? source`
+			${EMOJIS.WARN} Missing permissions to delete messages:
+			  ${links.join("\n")}
+		`
+		: "";
+
+	const embeds = messages.map((message) => messageToEmbed(message));
+
+	const res = await yesNo({
+		data: {
+			content: [
+				"Here is a preview of what will be archived. Do you want to continue?",
+				missingPerms
+			]
+				.join("\n\n")
+				.trim(),
+			embeds
+		},
+		medium: interaction,
+		filter: (i) => i.user.id === interaction.user.id
+	});
+
+	if (!res) {
+		return interaction.editReply({
+			components: [],
+			content: `${EMOJIS.SPARKS} Alright! Cancelled archiving pins.`,
+			embeds: []
+		});
+	}
+
+	const row1 = new ActionRowBuilder<ChannelSelectMenuBuilder>().setComponents(
+		components.selects.channelSelect.component()
+	);
+
+	const row2 = new ActionRowBuilder<ButtonBuilder>().setComponents(
+		components.buttons.back.component()
+	);
+
+	const msg = await interaction.editReply({
+		components: [row1, row2],
+		content: "Select the channel to archive in.",
+		embeds: []
+	});
+
+	const component = await msg
+		.awaitMessageComponent({
+			filter: (i) => i.user.id === interaction.user.id,
+			time: 60_000
+		})
+		.catch(() => null);
+
+	if (!component?.isChannelSelectMenu()) {
+		return interaction.editReply({
+			components: [],
+			content: `${EMOJIS.SPARKS} Alright! Cancelled archiving pins.`,
+			embeds: []
+		});
+	}
+
+	const channelId = component.values[0];
+	const channel = interaction.guild.channels.cache.get(channelId) as
+		| NewsChannel
+		| TextChannel
+		| undefined;
+
+	await interaction.channel?.bulkDelete(messages);
+
+	for (const embed of embeds) {
+		await channel?.send({ embeds: [embed] });
+	}
 };
 
 export const getCommand: () => Command = () => ({
 	data,
-	run
+	handle: {
+		contextMenu
+	}
 });
