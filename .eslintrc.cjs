cd57bc281880b2eef7827c41710da72c879f6f31
el-durango <draaangooo@gmail.com>
2023-03-12 15:51:40 +0100

    chore: update and refactor ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,7 +25,12 @@ module.exports = {
 		"@typescript-eslint/array-type": ["error", { default: "generic" }],
 		"@typescript-eslint/await-thenable": "error",
 		"@typescript-eslint/consistent-type-definitions": "error",
-		"@typescript-eslint/consistent-type-imports": "error",
+		"@typescript-eslint/consistent-type-imports": [
+			"error",
+			{
+				fixStyle: "inline-type-imports"
+			}
+		],
 		"@typescript-eslint/explicit-member-accessibility": "error",
 		"@typescript-eslint/member-ordering": "error",
 		"@typescript-eslint/method-signature-style": ["error", "method"],
@@ -33,30 +38,32 @@ module.exports = {
 		"@typescript-eslint/no-inferrable-types": "error",
 		"@typescript-eslint/no-meaningless-void-operator": "error",
 		"@typescript-eslint/no-misused-promises": "error",
-		"@typescript-eslint/no-non-null-assertion": "off",
+		"@typescript-eslint/no-non-null-asserted-nullish-coalescing": "error",
+		"@typescript-eslint/no-non-null-assertion": "error",
+		"@typescript-eslint/no-unnecessary-condition": "error",
+		"@typescript-eslint/no-unnecessary-type-arguments": "error",
 		"@typescript-eslint/no-unnecessary-type-assertion": "error",
 		"@typescript-eslint/prefer-optional-chain": "error",
+		"@typescript-eslint/promise-function-async": "error",
 		"@typescript-eslint/prefer-readonly": "error",
 		"@typescript-eslint/require-await": "error",
 		"@typescript-eslint/sort-type-union-intersection-members": "error",
-
+		"@typescript-eslint/switch-exhaustiveness-check": "error",
 		"arrow-body-style": ["error", "as-needed"],
+		"consistent-return": "error",
 		"curly": "error",
 		"eqeqeq": ["error", "smart"],
-
+		"no-constant-binary-expression": "error",
+		"no-constructor-return": "error",
 		"no-duplicate-imports": "error",
 		"no-else-return": "error",
 		"no-extra-boolean-cast": "error",
 		"no-implicit-coercion": "error",
 		"no-invalid-regexp": "error",
 		"no-lonely-if": "error",
+		"no-multi-assign": "error",
+		"no-new-native-nonconstructor": "error",
 		"no-param-reassign": "error",
-		"no-unneeded-ternary": "error",
-		"no-useless-concat": "error",
-		"no-useless-escape": "error",
-		"no-useless-rename": "error",
-		"no-useless-return": "error",
-		"no-var": "error",
 		"no-restricted-globals": [
 			"error",
 			...builtinModules.map((moduleName) => ({
@@ -64,7 +71,14 @@ module.exports = {
 				message: `Import from \`node:${moduleName}\` instead.`
 			}))
 		],
-
+		"no-self-compare": "error",
+		"no-unneeded-ternary": "error",
+		"no-unused-private-class-members": "error",
+		"no-useless-concat": "error",
+		"no-useless-escape": "error",
+		"no-useless-rename": "error",
+		"no-useless-return": "error",
+		"no-var": "error",
 		"object-curly-newline": [
 			"error",
 			{
@@ -81,7 +95,6 @@ module.exports = {
 			}
 		],
 		"object-shorthand": "error",
-
 		"padding-line-between-statements": [
 			"error",
 			{
@@ -93,17 +106,17 @@ module.exports = {
 				blankLine: "always",
 				next: "*",
 				prev: [
-					"multiline-expression",
 					"multiline-block-like",
 					"multiline-const",
+					"multiline-expression",
 					"multiline-let"
 				]
 			}
 		],
-
+		"prefer-arrow-callback": "error",
 		"prefer-const": "error",
 		"prefer-template": "error",
-
-		"quotes": ["error", "double", { avoidEscape: true }]
+		"quotes": ["error", "double", { avoidEscape: true }],
+		"yoda": ["error", "never"]
 	}
 };
